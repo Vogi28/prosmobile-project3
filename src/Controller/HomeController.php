3ee3ee2dcc2317e88933de63a0cb14c59fd4d127
Jean-Marie TRUONG <jean-marie_truong@outlook.fr>
2020-01-13 13:43:41 +0100

    ajout slide promo

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\PromoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -15,6 +17,16 @@ class HomeController extends AbstractController
         
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home", methods={"GET"})
+     */
+    public function homeSlider(PromoRepository $promoRepository): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'promos' => $promoRepository->findAll(),
         ]);
     }
 }
